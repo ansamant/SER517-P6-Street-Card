2b831ceb13ca58cb164b9f9b4067e7d0d55b8c39
sverma23x <shivam920302@gmail.com>
2020-02-08 16:01:24 -0700

    US#3 Task#40 - Created Serializer for Social Worker and Default User(Django) to serialize data to JSON.

--- api/StreetCardServices/serializers.py
+++ api/StreetCardServices/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from .models import SocialWorker
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +14,31 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class SocialWorkerSerializer(ModelSerializer):
+    clearanceLevel = serializers.CharField()
+    address = serializers.CharField()
+    serviceProvider = serializers.CharField()
+
+    class Meta:
+        model = SocialWorker
+        fields = ('id', 'clearanceLevel', 'address', 'serviceProvider')
+
+
+class UserSerializer(ModelSerializer):
+    socialWorker = SocialWorkerSerializer(required=False)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password', 'socialWorker')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('socialWorker')
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        SocialWorker.objects.create(user=user, **profile_data)
+        return user
