e3406853990aab35c81d30af37848cafe28dc7b8
nkonchad <nkonchad@asu.edu>
2020-02-08 20:23:56 -0700

    US#1 Task#21 - Configured authentication access in settings for the system

--- api/settings.py
+++ api/settings.py
@@ -119,7 +119,15 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
 
 SIMPLE_JWT = {
