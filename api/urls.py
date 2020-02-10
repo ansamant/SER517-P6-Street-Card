59bdbc082c4966ac4844173e191a682af15143ac
nkonchad <nkonchad@asu.edu>
2020-02-09 17:59:51 -0700

    US#1 Task#44 -Added endpoints in routes for receiving refresh and access tokens

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,7 @@ from django.urls import include, path
 from rest_framework import routers
 from api.StreetCardServices import views
 from api.StreetCardServices.views import SocialWorkerRegistration, SocialWorkerDetails
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 
 router = routers.DefaultRouter()
@@ -30,5 +31,7 @@ router.register('socialinfo', SocialWorkerDetails)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
