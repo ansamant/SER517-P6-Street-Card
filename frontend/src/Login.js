6828454561736fe6e3de7bffc799798da1c62393
AkashKadam75 <kadamakash75@gmail.com>
2020-02-12 14:27:24 -0700

    US#1 Task#46 - integration of login page to backend API

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,44 +5,24 @@ import './index.css';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import Header from './HeaderCommon.js'
 import StreetCardFooter from './StreetCardFooter'
+import WrappedRegistrationForm from './Registration'
 
  class NormalLoginForm extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      logged_in: localStorage.getItem('token') ? true : false,
+      displayed_form: 'login',
+      logged_in: localStorage.getItem('access_token') ? true : false,
       username: ''
     };
   }
-
-
-  handle_login = (e, data) => {
-    e.preventDefault();
-    fetch('http://localhost:8000/token-auth/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(json => {
-        localStorage.setItem('token', json.token);
-        this.setState({
-          logged_in: true,
-          displayed_form: '',
-          username: json.user.username
-        });
-      });
-  };
 
 
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
         fetch('http://localhost:8000/api/token/', {
           method: 'POST',
           headers: {
@@ -52,10 +32,12 @@ import StreetCardFooter from './StreetCardFooter'
         })
           .then(res => res.json())
           .then(json => {
-            localStorage.setItem('token', json.token);
+            localStorage.setItem('access_token', json.access);
+            localStorage.setItem('refresh_token', json.refresh);
             this.setState({
+              displayed_form: 'signup',
               logged_in: true,
-              username: json.user.username
+              username: values.username
             });
           });
         }
@@ -64,39 +46,51 @@ import StreetCardFooter from './StreetCardFooter'
 
   render() {
     const { getFieldDecorator } = this.props.form;
+
+    let form;
+    switch (this.state.displayed_form) {
+      case 'login':
+        form = <div>
+                  <Header/>
+                  <Form onSubmit={this.handleSubmit} className="login-form">
+                    <Form.Item>
+                      {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username!' }],
+                      })(
+                        <Input
+                          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                          placeholder="Username"
+                        />,
+                      )}
+                    </Form.Item>
+                    <Form.Item>
+                      {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input your Password!' }],
+                      })(
+                        <Input
+                          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                          type="password"
+                          placeholder="Password"
+                        />,
+                      )}
+                    </Form.Item>
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                  <StreetCardFooter/>
+                </div>;
+        break;
+      case 'signup':
+        form = <WrappedRegistrationForm />;
+        break;
+      default:
+        form = null;
+    }
     return (
-      <div>
-        <Header/>
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Username"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              Log in
-            </Button>
-          </Form.Item>
-        </Form>
-        <StreetCardFooter/>
-      </div>
+      <div>{form}</div>
     );
   }
 }
