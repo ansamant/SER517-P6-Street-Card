6dd5757da87e7e724ffc1f74ebd30d49a8ef7555
AkashKadam75 <kadamakash75@gmail.com>
2020-03-05 16:01:53 -0700

    US#109 Task #117

--- frontend/src/Registration.js
+++ frontend/src/Registration.js
@@ -11,13 +11,19 @@ import {
   AutoComplete,
   Icon,
   Row,
-  Col
+  Col,
+  Layout,
+  Menu
 } from "antd";
 import Header from "./Header";
 import StreetCardFooter from './StreetCardFooter'
 
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
 const { Option } = Select;
 const AutoCompleteOption = AutoComplete.Option;
+
+const { Content, Sider } = Layout;
 
 const clearanceLevel = [
   {
@@ -199,171 +205,198 @@ class RegistrationForm extends React.Component {
       }
     };
 
-    if(this.props.clearanceLevel === "caseworker" && "shivamverma" !== this.props.username){
+    // if(this.props.clearanceLevel === "caseworker" && "shivamverma" !== this.props.username){
+      if(true){
       return (
-      <div>
-      <Header 
-        handleSuccessfulLogoutAction={this.handleSuccessfulLogoutAction}
-        loggedInStatus={this.state.loggedInStatus}
-      />
-      <div className="caseworker-homeless-field">
-        <Button onClick={this.homelessRegistration} className="caseworker-homeless-registration-button" type="primary" block>Register Homeless Person</Button>
-      </div>
-      <div className="personId-form-input">
-        <Form {...formItemLayout} onSubmit={this.handlePersonalIdSubmit.bind(this)} className="personId-form">
-          <Form.Item>
-            {getFieldDecorator("personId", {
-              rules: [
-                {
-                  required: true,
-                  message: "Please input Identification Number!",
-                  whitespace: true
-                }
-              ]
-            })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)'}} />} placeholder="Homeless Person Identification Number" />)}
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Row>
-              <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-                <Button type="primary" htmlType="submit" style={{ width: '150px', marginLeft: '160px' }}>
-                Edit
-              </Button>
-              </Col>
-              <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-                <Button type="primary" htmlType="submit"   onClick={this.viewLongs.bind(this)} style={{ width: '150px', marginLeft: '160px' }}>
-                  Logs Info
-                </Button>
-              </Col>
-            </Row>
-          </Form.Item>
-        </Form>
-      </div>
-      <StreetCardFooter/>
-      </div>
+      <Layout className="layout">
+        <Header 
+          handleSuccessfulLogoutAction={this.handleSuccessfulLogoutAction}
+          loggedInStatus={this.state.loggedInStatus}
+        />
+        <Layout>
+           <Sider className="site-layout-sider"
+          >
+            <Menu style={{ borderRight : '0px', backgroundColor: '#173e43' }} mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item style={{ marginTop: '20px', color: '#fae596'}} key="1">
+                <span onClick={this.homelessRegistration} >Register Client</span>
+              </Menu.Item>
+              <Menu.Item style={{ marginTop: '20px', color: '#fae596'}} key="2">
+                <span>Update Client Information</span>
+              </Menu.Item>
+              <Menu.Item style={{ marginTop: '20px', color: '#fae596'}} key="3">
+                <span>Schedule Appointment</span>
+              </Menu.Item>
+              <Menu.Item style={{ marginTop: '20px', color: '#fae596'}} key="4">
+                <span>Update Appointment</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Content className="content">
+            <div className="caseworker-homeless-field">
+              <Button onClick={this.homelessRegistration} className="caseworker-homeless-registration-button" type="primary" block>Register Homeless Person</Button>
+            </div>
+            <div className="personId-form-input">
+              <Form {...formItemLayout} onSubmit={this.handlePersonalIdSubmit.bind(this)} className="personId-form">
+                <Form.Item>
+                  {getFieldDecorator("personId", {
+                    rules: [
+                      {
+                        required: true,
+                        message: "Please input Identification Number!",
+                        whitespace: true
+                      }
+                    ]
+                  })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)'}} />} placeholder="Homeless Person Identification Number" />)}
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                  <Row>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+                      <Button type="primary" htmlType="submit" style={{ width: '150px', marginLeft: '160px' }}>
+                      Edit
+                    </Button>
+                    </Col>
+                    <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+                      <Button type="primary" htmlType="submit"   onClick={this.viewLongs.bind(this)} style={{ width: '150px', marginLeft: '160px' }}>
+                        Logs Info
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form.Item>
+              </Form>
+            </div>
+          </Content>
+        </Layout>
+        <StreetCardFooter/>
+      </Layout>
     );
     }else{
       return (
-      <div>
+      <Layout className="layout">
       <Header 
         handleSuccessfulLogoutAction={this.handleSuccessfulLogoutAction}
         loggedInStatus={this.state.loggedInStatus}
       />
-
-        <Form {...formItemLayout} onSubmit={this.handleSocialWorkerRegistrationSubmit} className="registration-form ">
-          <h1 style={{marginLeft: '90px'}}>Register Social Worker</h1>
-        <Form.Item>
-          {getFieldDecorator("username", {
-            initialValue: this.state.username,
-            rules: [
-              {
-                required: true,
-                message: "Please input your username!",
-                whitespace: true
-              }
-            ]
-          })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />)}
-        </Form.Item>
-        <Form.Item hasFeedback>
-          {getFieldDecorator("password", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your password!"
-              },
-              {
-                validator: this.validateToNextPassword
-              }
-            ]
-          })(<Input.Password  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Password"/>)}
-        </Form.Item>
-        <Form.Item hasFeedback>
-          {getFieldDecorator("confirm", {
-            rules: [
-              {
-                required: true,
-                message: "Please confirm your password!"
-              },
-              {
-                validator: this.compareToFirstPassword
-              }
-            ]
-          })(<Input.Password prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Confirm Password" onBlur={this.handleConfirmBlur} />)}
-        </Form.Item>
-        <Form.Item u>
-          {getFieldDecorator("email", {
-            rules: [
-              {
-                type: "email",
-                message: "The input is not valid E-mail!"
-              },
-              {
-                required: true,
-                message: "Please input your E-mail!"
-              }
-            ]
-          })(<Input prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="E-mail"/>)}
-        </Form.Item>
-        <Form.Item >
-          {getFieldDecorator("first_name", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your first name!",
-                whitespace: true
-              }
-            ]
-          })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="First Name"/>)}
-        </Form.Item>
-        <Form.Item >
-          {getFieldDecorator("last_name", {
-            rules: [
-              {
-                message: "Please input your last name!",
-                whitespace: true
-              }
-            ]
-          })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Last Name"/>)}
-        </Form.Item>
-        <Form.Item >
-          {getFieldDecorator("address", {
-            rules: [
-              {
-                message: "Please input your address!",
-                whitespace: true
-              }
-            ]
-          })(<Input prefix={<Icon type="home" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Address"/>)}
-        </Form.Item>
-        <Form.Item >
-          {getFieldDecorator("clearanceLevel", {
-            rules: [
-              {
-                type: "array",
-                required: true,
-                message: "Please select your role!"
-              }
-            ]
-          })(<Cascader options={clearanceLevel} placeholder="Clearence Level" />)}
-        </Form.Item>
-        <Form.Item >
-          {getFieldDecorator("serviceProvider", {
-            rules: [
-              {
-                type: "array",
-                required: true,
-                message: "Please select your role!"
-              }
-            ]
-          })(<Cascader options={serviceProvider} placeholder="Service Provider"/>)}
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit" className="registration-submit-button">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <Layout>
+        <Content className="content">
+          <div className="site-layout-content-registration">
+            <Form {...formItemLayout} onSubmit={this.handleSocialWorkerRegistrationSubmit} >
+              <h1 style={{marginLeft: '90px', marginLeft : '180px'}}>Register Social Worker</h1>
+              <Form.Item className="register-ant-form-item">
+                {getFieldDecorator("username", {
+                  initialValue: this.state.username,
+                  rules: [
+                    {
+                      required: true,
+                      message: "Please input your username!",
+                      whitespace: true
+                    }
+                  ]
+                })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />)}
+              </Form.Item>
+              <Form.Item hasFeedback className="register-ant-form-item">
+                {getFieldDecorator("password", {
+                  rules: [
+                    {
+                      required: true,
+                      message: "Please input your password!"
+                    },
+                    {
+                      validator: this.validateToNextPassword
+                    }
+                  ]
+                })(<Input.Password  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Password"/>)}
+              </Form.Item>
+              <Form.Item hasFeedback className="register-ant-form-item">
+                {getFieldDecorator("confirm", {
+                  rules: [
+                    {
+                      required: true,
+                      message: "Please confirm your password!"
+                    },
+                    {
+                      validator: this.compareToFirstPassword
+                    }
+                  ]
+                })(<Input.Password prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Confirm Password" onBlur={this.handleConfirmBlur} />)}
+              </Form.Item >
+              <Form.Item className="register-ant-form-item">
+                {getFieldDecorator("email", {
+                  rules: [
+                    {
+                      type: "email",
+                      message: "The input is not valid E-mail!"
+                    },
+                    {
+                      required: true,
+                      message: "Please input your E-mail!"
+                    }
+                  ]
+                })(<Input prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="E-mail"/>)}
+              </Form.Item>
+              <Form.Item className="register-ant-form-item">
+                {getFieldDecorator("first_name", {
+                  rules: [
+                    {
+                      required: true,
+                      message: "Please input your first name!",
+                      whitespace: true
+                    }
+                  ]
+                })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="First Name"/>)}
+              </Form.Item>
+              <Form.Item className="register-ant-form-item">
+                {getFieldDecorator("last_name", {
+                  rules: [
+                    {
+                      message: "Please input your last name!",
+                      whitespace: true
+                    }
+                  ]
+                })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Last Name"/>)}
+              </Form.Item>
+              <Form.Item className="register-ant-form-item">
+                {getFieldDecorator("address", {
+                  rules: [
+                    {
+                      message: "Please input your address!",
+                      whitespace: true
+                    }
+                  ]
+                })(<Input prefix={<Icon type="home" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Address"/>)}
+              </Form.Item>
+              <Form.Item className="register-ant-form-item">
+                {getFieldDecorator("clearanceLevel", {
+                  rules: [
+                    {
+                      type: "array",
+                      required: true,
+                      message: "Please select your role!"
+                    }
+                  ]
+                })(<Cascader options={clearanceLevel} placeholder="Clearence Level" />)}
+              </Form.Item>
+              <Form.Item className="register-ant-form-item">
+                {getFieldDecorator("serviceProvider", {
+                  rules: [
+                    {
+                      type: "array",
+                      required: true,
+                      message: "Please select your role!"
+                    }
+                  ]
+                })(<Cascader options={serviceProvider} placeholder="Service Provider"/>)}
+              </Form.Item>
+              <Form.Item {...tailFormItemLayout} className="register-ant-form-item">
+                <Button type="primary" htmlType="submit" className="registration-submit-button">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Content>
+      </Layout>
       <StreetCardFooter/>
-      </div>
+    </Layout>
     );
     }
     
