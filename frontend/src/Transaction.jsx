6de3eebb4a4e9f0ac4d998fc8da9191a793dcce1
Prashansa <pprasha2@asu.edu>
2020-04-05 13:52:03 -0700

    US#10 Task#202 #203 #204 -- Calculating the amount of transaction and displaying it in a dynamic table.

--- frontend/src/Transaction.jsx
+++ frontend/src/Transaction.jsx
@@ -3,54 +3,18 @@ import 'antd/dist/antd.css';
 import './index.css';
 import {Button, Form, Layout, Table, InputNumber} from "antd";
 import Header from './Header'
+import './transaction.css'
 import StreetCardFooter from './StreetCardFooter'
 
 const {Content} = Layout;
 
 class Transaction extends React.Component {
     constructor(props) {
-
-
         super(props);
         this.state = {
             isLoaded: false,
             selectedRowKeys: [],
-            columns: [
-                {
-                    title: 'Product Id',
-                    dataIndex: 'productId',
-                    key: 'productId',
-                    width: 80,
-                },
-                {
-                    title: 'Product Name',
-                    dataIndex: 'productName',
-                    key: 'productName',
-                    width: 30,
-                },
-                {
-                    title: 'Cost Per Item',
-                    dataIndex: 'costPerItem',
-                    key: 'costPerItem',
-                    width: 30,
-                },
-                {
-                    title: 'Units Available',
-                    dataIndex: 'unitsAvailable',
-                    key: 'unitsAvailable',
-                    width: 30,
-                },
-                {
-                    title: 'Units Given',
-                    key: 'action',
-                    width: 30,
-                    render: () => (
-                        <span>
-                        <InputNumber min={0} max={100} defaultValue={0}/>
-                        </span>
-                    ),
-                },
-            ],
+            totalAmount: 0,
             productData: [
                 {
                     productId: '',
@@ -58,12 +22,26 @@ class Transaction extends React.Component {
                     costPerItem: '',
                     unitsAvailable: '',
                     serviceProvider: '',
+                    quantity: 0,
+                    amount: 0,
+                    index1: 0
                 }
             ]
+
         }
         this.handleSuccessfulLogoutAction = this.handleSuccessfulLogoutAction.bind(this);
         this.setPagecomponent = this.setPagecomponent.bind(this);
     }
+
+    takeIntput = (e, index) => {
+
+        let prodData = JSON.parse(JSON.stringify(this.state.productData));
+        prodData[index].quantity = e.target.value;
+        prodData[index].amount = e.target.value *  prodData[index].costPerItem;
+        this.setState({productData: prodData})
+        this.state.totalAmount += prodData[index].amount;
+    }
+
 
     componentDidMount() {
         fetch('http://localhost:8000/product/', {
@@ -75,9 +53,17 @@ class Transaction extends React.Component {
         })
             .then(res => res.json())
             .then(json => {
+                let prod = [];
+                json.forEach((key, index) => {
+                    key = {...key, "unitsPurchased": 0,
+                        "amount": 0 , "index1" : index  }
+                        prod.push(key)
+
+                })
+                console.log(prod)
                 this.setState({
                         isLoaded: true,
-                        productData: json,
+                        productData: prod,
                     }
                 )
             })
@@ -90,38 +76,39 @@ class Transaction extends React.Component {
     }
 
     setPagecomponent(pageComponentValue) {
-        this.props.updatePageComponent(pageComponentValue)
+        this.props.updatePageComponent(pageComponentValue);
         this.props.history.push('/socialWorkerRegister');
-    };
+    }
 
-    start = () => {
-        this.setState({ loading: true });
-        // ajax request after empty completing
-        setTimeout(() => {
-        this.setState({
-        selectedRowKeys: [],
-        loading: false,
-        });
-        }, 1000);
-    };
+    renderTableHeader= () => {
+        let head = {
 
-    onSelectChange = selectedRowKeys => {
-        console.log('selectedRowKeys changed: ', selectedRowKeys);
-        this.setState({ selectedRowKeys });
-    };
+        }
+      let header =["Product Id", "Product Name","Cost Per Item", "Units Available","Service Provider","Given Units", "Amount"]
+      return header.forEach((key, index) => {
+          console.log("header",key);
+         return (<th key={index}>{key.toUpperCase()}</th>)
+      })
+   }
 
-
-    onChange(value) {
-  console.log('changed', value);
-}
+    renderTableData() {
+      return this.state.productData.map((product, index) => {
+         const { productId, productName, costPerItem, unitsAvailable, serviceProvider, amount, index1} = product //destructuring
+         return (
+            <tr key={productId}>
+               <td align={"center"}>{productId}</td>
+               <td align={"center"}>{productName}</td>
+               <td align={"center"}>{costPerItem}</td>
+               <td align={"center"}>{unitsAvailable}</td>
+               <td align={"center"}>{serviceProvider}</td>
+               <td><InputNumber min={0} max={100} defaultValue={0} onBlur={(e) => this.takeIntput(e,index1)}/></td>
+               <td>{amount}</td>
+            </tr>
+         )
+      })
+   }
 
     render() {
-        const { loading, selectedRowKeys } = this.state;
-        const rowSelection = {
-            selectedRowKeys,
-            onChange: this.onSelectChange,
-            };
-        const hasSelected = selectedRowKeys.length > 0;
         console.log(this.props.homelessPersonId)
         return (
             <Layout className="layout">
@@ -131,8 +118,14 @@ class Transaction extends React.Component {
                 <Layout>
                     <Content className="content-enroll">
                         <div className="site-layout-content-homeless">
-                            <Table rowSelection={rowSelection} className="site-layout-content-viewappointment"
-                                   dataSource={this.state.productData} columns={this.state.columns}/>
+                            <h1 align="center">Inventory Details</h1>
+                            <table id='inventory'>
+                                <tbody >
+                                    <tr>{this.renderTableHeader}</tr>
+                                    {this.renderTableData()}
+                                </tbody>
+                            </table>
+                            <p align={"center"}>Total Amount : { this.state.totalAmount }</p>
                         </div>
                     </Content>
                 </Layout>
