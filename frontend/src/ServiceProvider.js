c868ed5a4fc30027e32c389b0429d2dcd26ea859
AkashKadam75 <kadamakash75@gmail.com>
2020-04-10 09:32:42 -0700

    US#13 Task#197 Task#213 combined chart

--- frontend/src/ServiceProvider.js
+++ frontend/src/ServiceProvider.js
@@ -338,6 +338,53 @@ class ServiceProviderView extends React.Component {
             </Layout>
                 </div>
             );
+        }else if(this.state.pageComponent == 'combinedChart'){
+            return(
+                <div>
+                <Layout className="layout">
+            <Header handleSuccessfulLogoutAction={this.handleSuccessfulLogoutAction}
+                    loggedInStatus={this.state.loggedInStatus}
+            />
+            <Layout>
+                <SiderComponentServiceProvider
+                    setPagecomponent={this.setPagecomponent}
+                />
+                <Content className="content-login">
+                        <Chart
+                          className = "site-layout-content-graphs"
+                          chartType="ComboChart"
+                          loader={<div>Loading Chart</div>}
+                          data={[
+                            [
+                              'Month',
+                              'Bolivia',
+                              'Ecuador',
+                              'Madagascar',
+                              'Papua New Guinea',
+                              'Rwanda',
+                              'Average',
+                            ],
+                            ['2004/05', 165, 938, 522, 998, 450, 614.6],
+                            ['2005/06', 135, 1120, 599, 1268, 288, 682],
+                            ['2006/07', 157, 1167, 587, 807, 397, 623],
+                            ['2007/08', 139, 1110, 615, 968, 215, 609.4],
+                            ['2008/09', 136, 691, 629, 1026, 366, 569.6],
+                          ]}
+                          options={{
+                            title: 'Monthly Coffee Production by Country',
+                            vAxis: { title: 'Cups' },
+                            hAxis: { title: 'Month' },
+                            seriesType: 'bars',
+                            series: { 5: { type: 'line' } },
+                          }}
+                          rootProps={{ 'data-testid': '1' }}
+                        />
+                </Content>
+            </Layout>
+            <StreetCardFooter/>
+            </Layout>
+                </div>
+            );
         }else {
             return(
             <Layout className="layout">
