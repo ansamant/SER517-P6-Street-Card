9edea020f9362972a69ed62f1ef5f39893c4683d
Prashansa <pprasha2@asu.edu>
2020-04-21 09:57:43 -0700

    US#220 Task#241 --Created Post function to add a new product in the inventory.

--- frontend/src/AddProduct.js
+++ frontend/src/AddProduct.js
@@ -65,15 +65,37 @@ class AddProduct extends React.Component {
 
     }
 
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                var newProduct = {};
+                newProduct.productName = values.productName;
+                newProduct.category = values.category[0];
+                newProduct.unitsAvailable = values.unitsAvailable;
+                newProduct.costPerItem = values.costPerItem;
+                newProduct.serviceProvider = values.serviceProvider[0];
+                console.log("New product details: ",newProduct);
+                fetch('http://localhost:8000/product/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify(newProduct)
+                })
+                    .then(res => res.json())
+            }
 
-
+        });
+    }
 
     render() {
         const {getFieldDecorator} = this.props.form;
         return (
                     <Content>
                         <div>
-                            <Form >
+                            <Form onSubmit={this.handleSubmit}>
                                 <Form.Item>
                                     {getFieldDecorator("productName", {
                                         rules: [
@@ -88,14 +110,13 @@ class AddProduct extends React.Component {
                                     {getFieldDecorator("category", {
                                         rules: [
                                             {
-                                               type: "array",
                                                required: true,
                                                message: "Please select the category of the product!"
                                             }
                                         ]
                                     })(<Cascader options={category} placeholder="Category"/>)}
                                 </Form.Item>
-                                <Form.Item>
+                                <Form.Item style={{width:'30%'}}>
                                     {getFieldDecorator("unitsAvailable", {
                                         rules: [
                                             {
@@ -105,11 +126,20 @@ class AddProduct extends React.Component {
                                         ]
                                     })(<InputNumber min={1} placeholder="Number of units"/>)}
                                 </Form.Item>
+                                <Form.Item style={{width:'30%'}}>
+                                    {getFieldDecorator("costPerItem", {
+                                        rules: [
+                                            {
+                                               required: true,
+                                               message: "Please input the price per unit!"
+                                            }
+                                        ]
+                                    })(<InputNumber min={0.00} step={0.01} placeholder="Cost Per Item"/>)}
+                                </Form.Item>
                                 <Form.Item>
                                     {getFieldDecorator("serviceProvider", {
                                         rules: [
                                             {
-                                               type: "array",
                                                required: true,
                                                message: "Please select the your service provider!"
                                             }
